5e67d5f408e35ead8ee259cb2392431f28886c5f
Marko193 <marko.pavlenko@nure.ua>
2021-03-18 15:03:00 +0200

    Created the endpoint for the follow button

--- app.js
+++ app.js
@@ -31,12 +31,15 @@ const logoutRoute = require('./routes/logout');
 
 // Api routes
 const postsApiRoute = require('./routes/api/posts');
+const usersApiRoute = require('./routes/api/users');
 
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/posts", middleware.requireLogin, postRoute);
 app.use("/profile", middleware.requireLogin, profileRoute);
+
 app.use("/api/posts", postsApiRoute);
+app.use("/api/users", usersApiRoute);
 
 app.use('/logout', logoutRoute);
 
